07130b67bd11a5d5cbd21eb8da210f08c329b4a6
HOOR ALMASHHOUR <houralmashhour@gmail.com>
2025-11-12 02:10:48 +0300

    update

--- app/MyAccountPage/page.tsx
+++ app/MyAccountPage/page.tsx
@@ -1,6 +1,8 @@
+'use client';
 
 import LoginSection from "../src/Components/LoginSection/LoginSection";
 
+console.log('✅ Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
 
 
 export default function MyAccountPage() {
@@ -8,6 +10,7 @@ export default function MyAccountPage() {
     <div className=" bg-gray-100 py-40 px-4">
       <h1 className="text-3xl font-bold text-center mb-8 text-[#6B3074]">حسابي </h1>
       <LoginSection />
+      
     </div>
     );
 }
